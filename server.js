244f34c484f38b87b10bb2aded0b50ea735a4a51
Luis Febro <mr.febro@gmail.com>
2020-09-24 19:08:14 -0400

    part 3

--- server.js
+++ server.js
@@ -3,25 +3,40 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+// bring routes
+const blogRoutes = require('./routes/blog');
 
 // app
 const app = express();
 
 // middlewares
 app.use(morgan('dev'));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cookieParser());
+
+// db
+mongoose
+    .connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+    .then(() => console.log('DB connected'));
+
 // cors
-app.use(cors());
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 // routes
 app.get('/api', (req, res) => {
     res.json({ time: Date().toString() });
 });
 
+// routes middleware
+app.use('/api', blogRoutes);
+
 // port
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
